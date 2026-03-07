37b9031594f6bd41fe062ce84913af3cf37fd9f8
Sensemore <mdsagakpe@st.ug.edu.gh>
2026-03-07 23:48:13 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/authRoutes.js";
+import taskRoutes from "./routes/taskRoutes.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.log(err));
+
+app.use("/api/auth", authRoutes);
+app.use("/api/tasks", taskRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
